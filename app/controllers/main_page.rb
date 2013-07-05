93aa1fa2bf3b3a537d3be5d63ddb3f05148a192a
Jared Smith <jared.rit@me.com>
2013-07-05 15:43:19 -0700

    Modify the your_surveys partial page.

--- app/controllers/main_page.rb
+++ app/controllers/main_page.rb
@@ -2,33 +2,3 @@ get '/main' do
   @created_surveys = current_user.surveys
 	erb :main
 end
-
-
-before '/stats' do 
-	redirect '/' unless logged_in?
-end
-
-get '/stats' do 
-	erb :'partials/_stats'
-end
-
-
-before '/your_surveys' do 
-	redirect '/' unless logged_in?
-end
-
-get '/your_surveys' do 
-
-	erb :'partials/_your_surveys'
-end
-
-
-before '/take_surveys' do 
-	redirect '/' unless logged_in?
-end
-
-get '/take_surveys' do 
-
-	erb :'partials/_take_surveys'
-end
-
