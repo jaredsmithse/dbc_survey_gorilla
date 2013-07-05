05c0ed10530bb305282861a71abb3f4394fdaabf
Jared Smith <jared.rit@me.com>
2013-07-05 16:55:19 -0700

    Add functionality for swapping partials to create new survey in the 'your surveys' blade.

--- app/controllers/main_page.rb
+++ app/controllers/main_page.rb
@@ -2,3 +2,7 @@ get '/main' do
   @created_surveys = current_user.surveys
 	erb :main
 end
+
+post '/new_survey' do 
+	erb :'partials/_new_survey', :layout => false
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,12 @@
+function switchOutSurvey(HTMLResponse) {
+	$('.your-survey-content').replaceWith(HTMLResponse);
+}
+
 $(document).ready(function() {
-  $('.blades').on('click', function() { $('.blades').removeClass('active'); $(this).addClass('active'); })
+	$('.blades').on('click', function() { $('.blades').removeClass('active'); $(this).addClass('active'); })
+	$('#create').on('click', function(e) { 
+		var input = $(this).serialize();
+		e.preventDefault();
+		$.post('/new_survey', input, switchOutSurvey); 
+	});
 });
